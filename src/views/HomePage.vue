a1c05d4a1c8ca3072ef948f6048a26049b5ccad1
syoee <tjsdyd1347@gmail.com>
2024-08-06 21:12:46 +0900

    ADD: pagination

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,66 +2,31 @@
 	<div class="m-10">
 		<ul class="grid grid-flow-row gap-5">
 			<li
-				@click="goBoard"
+				v-for="post in paginatedPosts"
+				:key="post.id"
+				@click="goBoard(post.id)"
 				class="cursor-pointer hover:underline hover:underline-offset-4"
 			>
-				1. 첫번째 게시글
-			</li>
-			<li
-				@click="goBoard"
-				class="cursor-pointer hover:underline hover:underline-offset-4"
-			>
-				2. 두번째 게시글
-			</li>
-			<li
-				@click="goBoard"
-				class="cursor-pointer hover:underline hover:underline-offset-4"
-			>
-				3. 세번째 게시글
-			</li>
-			<li
-				@click="goBoard"
-				class="cursor-pointer hover:underline hover:underline-offset-4"
-			>
-				4. 네번째 게시글
-			</li>
-			<li
-				@click="goBoard"
-				class="cursor-pointer hover:underline hover:underline-offset-4"
-			>
-				5. 다섯번째 게시글
-			</li>
-			<li
-				@click="goBoard"
-				class="cursor-pointer hover:underline hover:underline-offset-4"
-			>
-				6. 여섯번째 게시글
-			</li>
-			<li
-				@click="goBoard"
-				class="cursor-pointer hover:underline hover:underline-offset-4"
-			>
-				7. 일곱번째 게시글
-			</li>
-			<li
-				@click="goBoard"
-				class="cursor-pointer hover:underline hover:underline-offset-4"
-			>
-				8. 여덟번째 게시글
-			</li>
-			<li
-				@click="goBoard"
-				class="cursor-pointer hover:underline hover:underline-offset-4"
-			>
-				9. 아홉번째 게시글
-			</li>
-			<li
-				@click="goBoard"
-				class="cursor-pointer hover:underline hover:underline-offset-4"
-			>
-				10. 열번째 게시글
+				{{ post.id }}. {{ post.title }}
 			</li>
 		</ul>
+		<div class="flex justify-between items-center mt-5">
+			<button
+				@click="prevPage"
+				:disabled="currentPage === 1"
+				class="px-3 py-2 bg-gray-300 text-black rounded-lg"
+			>
+				이전
+			</button>
+			<span>페이지 {{ currentPage }}</span>
+			<button
+				@click="nextPage"
+				:disabled="currentPage === totalPages"
+				class="px-3 py-2 bg-gray-300 text-black rounded-lg"
+			>
+				다음
+			</button>
+		</div>
 	</div>
 	<div class="flex justify-end mr-10">
 		<button
@@ -78,12 +43,57 @@ import { goBoard, goPost } from "@/utils/navigation";
 
 export default {
 	name: "HomePage",
+	data() {
+		return {
+			posts: [
+				{ id: 1, title: "첫 번째 게시글" },
+				{ id: 2, title: "두 번째 게시글" },
+				{ id: 3, title: "세 번째 게시글" },
+				{ id: 4, title: "네 번째 게시글" },
+				{ id: 5, title: "다섯 번째 게시글" },
+				{ id: 6, title: "여섯 번째 게시글" },
+				{ id: 7, title: "일곱 번째 게시글" },
+				{ id: 8, title: "여덟 번째 게시글" },
+				{ id: 9, title: "아홉 번째 게시글" },
+				{ id: 10, title: "열 번째 게시글" },
+				{ id: 11, title: "열한 번째 게시글" },
+				{ id: 12, title: "열두 번째 게시글" },
+				{ id: 13, title: "열세 번째 게시글" },
+				{ id: 14, title: "열네 번째 게시글" },
+				{ id: 15, title: "열다섯 번째 게시글" },
+				{ id: 16, title: "열여섯 번째 게시글" },
+				{ id: 17, title: "열일곱 번째 게시글" },
+			],
+			currentPage: 1,
+			postsPerPage: 10,
+		};
+	},
+	computed: {
+		totalPages() {
+			return Math.ceil(this.posts.length / this.postsPerPage);
+		},
+		paginatedPosts() {
+			const start = (this.currentPage - 1) * this.postsPerPage;
+			const end = start + this.postsPerPage;
+			return this.posts.slice(start, end);
+		},
+	},
 	methods: {
-		goBoard() {
-			goBoard(this.$router);
+		goBoard(postId) {
+			goBoard(this.$router, postId);
 		},
 		goPost() {
 			goPost(this.$router);
+		},
+		nextPage() {
+			if (this.currentPage < this.totalPages) {
+				this.currentPage++;
+			}
+		},
+		prevPage() {
+			if (this.currentPage > 1) {
+				this.currentPage--;
+			}
 		},
 	},
 };
