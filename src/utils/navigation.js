89825bc5ec343fa4d103e235eb03658493c3beb5
syoee <tjsdyd1347@gmail.com>
2024-08-06 20:48:11 +0900

    ADD: utils

---
+++ src/utils/navigation.js
@@ -0,0 +1,7 @@
+export function goBoard(router) {
+	router.push("/board");
+}
+
+export function goPost(router) {
+	router.push("/post");
+}
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,66 @@
 <template>
-	<div class="flex flex-col justify-center items-center mt-10">
-		<ul>
-			<li>111</li>
-			<li>222</li>
-			<li>333</li>
-			<li>444</li>
-			<li>555</li>
-			<li>666</li>
-			<li>777</li>
+	<div class="m-10">
+		<ul class="grid grid-flow-row gap-5">
+			<li
+				@click="goBoard"
+				class="cursor-pointer hover:underline hover:underline-offset-4"
+			>
+				1. 첫번째 게시글
+			</li>
+			<li
+				@click="goBoard"
+				class="cursor-pointer hover:underline hover:underline-offset-4"
+			>
+				2. 두번째 게시글
+			</li>
+			<li
+				@click="goBoard"
+				class="cursor-pointer hover:underline hover:underline-offset-4"
+			>
+				3. 세번째 게시글
+			</li>
+			<li
+				@click="goBoard"
+				class="cursor-pointer hover:underline hover:underline-offset-4"
+			>
+				4. 네번째 게시글
+			</li>
+			<li
+				@click="goBoard"
+				class="cursor-pointer hover:underline hover:underline-offset-4"
+			>
+				5. 다섯번째 게시글
+			</li>
+			<li
+				@click="goBoard"
+				class="cursor-pointer hover:underline hover:underline-offset-4"
+			>
+				6. 여섯번째 게시글
+			</li>
+			<li
+				@click="goBoard"
+				class="cursor-pointer hover:underline hover:underline-offset-4"
+			>
+				7. 일곱번째 게시글
+			</li>
+			<li
+				@click="goBoard"
+				class="cursor-pointer hover:underline hover:underline-offset-4"
+			>
+				8. 여덟번째 게시글
+			</li>
+			<li
+				@click="goBoard"
+				class="cursor-pointer hover:underline hover:underline-offset-4"
+			>
+				9. 아홉번째 게시글
+			</li>
+			<li
+				@click="goBoard"
+				class="cursor-pointer hover:underline hover:underline-offset-4"
+			>
+				10. 열번째 게시글
+			</li>
 		</ul>
 	</div>
 	<div class="flex justify-end mr-10">
@@ -21,12 +74,16 @@
 </template>
 
 <script>
+import { goBoard, goPost } from "@/utils/navigation";
+
 export default {
 	name: "HomePage",
-
 	methods: {
+		goBoard() {
+			goBoard(this.$router);
+		},
 		goPost() {
-			this.$router.push("/post");
+			goPost(this.$router);
 		},
 	},
 };
